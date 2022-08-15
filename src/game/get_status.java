e2b70230dd04d2f72465a5bd1d84734fb54891d8
mm1007 <m17mm1007.1007@gmail.com>
2022-08-15 23:05:26 +0900

    1.3.4

--- src/game/get_status.java
+++ src/game/get_status.java
@@ -1,7 +1,6 @@
 package game;
 
-public class get_status {
-
+abstract public class get_status {
 	/**
 	 * 指定したキーが押されているかbooleanで返します。
 	 * @param KeyCode　確認したいキーコード
@@ -10,6 +9,4 @@ public class get_status {
 	public static boolean get_key_press(int KeyCode) {
 		return Key.key[KeyCode];
 	}
-	
-	
 }
